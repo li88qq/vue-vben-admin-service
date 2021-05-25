ba578100fafdbe6d6cdd657e350a40df17e09369
li88qq <573258011@qq.com>
2021-05-25 23:10:55 +0800

    初始化项目

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,2 +1,12 @@
-package org.example;public class Application {
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
