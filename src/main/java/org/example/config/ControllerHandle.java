6f060f499655c0859d7ad954c5deaefc6b9e883f
li88qq <573258011@qq.com>
2021-06-09 22:25:05 +0800

    调整统一异常处理

--- src/main/java/org/example/config/ControllerHandle.java
+++ src/main/java/org/example/config/ControllerHandle.java
@@ -4,17 +4,19 @@ import org.example.dto.BaseResponse;
 import org.example.utils.ResponseUtil;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 统一异常处理
  */
 @ControllerAdvice
+@ResponseBody
 public class ControllerHandle {
 
-    @ExceptionHandler
+    @ExceptionHandler(value = Exception.class)
     public BaseResponse Exception() {
         //自定义异常
         //参数异常
-        return ResponseUtil.error("");
+        return ResponseUtil.error("操作失败");
     }
 }
