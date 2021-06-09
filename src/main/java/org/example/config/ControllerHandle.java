e59b84c4d9086df0c6c05caa6f05e85bae9a6780
li88qq <573258011@qq.com>
2021-06-09 22:57:10 +0800

    调整统一异常处理

--- src/main/java/org/example/config/ControllerHandle.java
+++ src/main/java/org/example/config/ControllerHandle.java
@@ -2,6 +2,8 @@ package org.example.config;
 
 import org.example.dto.BaseResponse;
 import org.example.utils.ResponseUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,9 +15,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @ResponseBody
 public class ControllerHandle {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ControllerHandle.class);
     @ExceptionHandler(value = Exception.class)
-    public BaseResponse Exception() {
+    public BaseResponse Exception(Exception e) {
         //自定义异常
+        LOG.error(e.getLocalizedMessage());
         //参数异常
         return ResponseUtil.error("操作失败");
     }
